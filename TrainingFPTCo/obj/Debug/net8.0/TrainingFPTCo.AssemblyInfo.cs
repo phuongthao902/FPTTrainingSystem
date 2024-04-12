cd95a491867fbe123892fe8967fa1a3c505ea895
tieumy <lephuongthaoit902@gmail.com>
2024-04-12 20:02:33 +0700

    Thaocommit

--- TrainingFPTCo/obj/Debug/net8.0/TrainingFPTCo.AssemblyInfo.cs
+++ TrainingFPTCo/obj/Debug/net8.0/TrainingFPTCo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingFPTCo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdcda2263bda9588a597402a33166bed05d130a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6067803ffd62ca22088c94e11dca19d04c52c4d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingFPTCo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingFPTCo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
